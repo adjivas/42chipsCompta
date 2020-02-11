051a8b24a221c6ba611c32352dea6550fbf37d10
Your Name <you@example.com>
2020-02-11 19:39:34 +0100

    Farnell one line

--- compta42chips/compta42chips.py
+++ compta42chips/compta42chips.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
-from pprint import pprint
+import json
 import pyFarnell
 
 f = pyFarnell.Farnell(apiKey='secret', region='fr.farnell.com')
-pprint(f.get_part_number('2507703'))
+dp = json.loads(f.get_part_number('2507703').get('_content')).get('premierFarnellPartNumberReturn')
+
+assert(dp.get("numberOfResults") == 1)
+
+product0 = dp.get("products")[0]
+
+assert(product0.get("productStatus") == "STOCKED")
+assert(product0.get("rohsStatusCode") == "YES_YES")
+
+datasheet0 = product0.get("datasheets")[0]
+prices = product0.get("prices")
+
+print("Datasheet", datasheet0.get("url"))
+print("Description", product0.get("displayName"))
+print("Fabricant", product0.get("brandName"))
+print("RefFabricant", product0.get("translatedManufacturerPartNumber"))
+print("Fournisseur", "Farnell")
+print("RefFournisseur", product0.get("sku"))
+print("costFournisseur", prices)
+print("deviceFournisseur", "EUR")
